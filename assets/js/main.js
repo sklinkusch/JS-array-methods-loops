9c1d5dc2deee340f49b35d6787ae4df648bb5b21
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-01-16 15:23:27 +0100

    shortened function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,11 @@
 function longestWordWithForLoop(string) {
   const words = string.split(" ");
-  let maxLength = 0;
-  let longestWord;
+  let longestWord = "";
   for (let numWords = 0; numWords < words.length; numWords++) {
-    if (words[numWords].length > maxLength) {
-      maxLength = words[numWords].length;
-      longestWord = words[numWords];
-    }
+    longestWord =
+      longestWord.length >= words[numWords].length
+        ? longestWord
+        : words[numWords];
   }
   return longestWord;
 }
